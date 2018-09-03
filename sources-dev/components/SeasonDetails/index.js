c59cec28a0b0a1b59f1823dd8155b04516ad02ab
fehd <fehdwelling@gmail.com>
2018-09-03 14:26:10 +0200

    - mise en place du image-on-top-scroll

--- sources-dev/components/SeasonDetails/index.js
+++ sources-dev/components/SeasonDetails/index.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    TextInput,
-    Button,
-    View,
-    FlatList,
-    Image
+    Text, View, Image, StatusBar, TouchableOpacity, FlatList
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { getSerieById } from '../../actions/index';
-import styles from './styles';
+import styles, {MIN_HEIGHT, MAX_HEIGHT} from './styles';
 import HeaderImageScrollView, { TriggeringView } from 'react-native-image-header-scroll-view';
+import * as Animatable from 'react-native-animatable';
+import Moment from 'moment';
 
 class SeasonDetails extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { showNavTitle: false };
+    }
 
     componentWillMount(){
         const { params } = this.props.navigation.state;
@@ -25,35 +25,83 @@ class SeasonDetails extends React.Component {
         this.props.getSerieById(idSerie)
     }
 
-    renderSeasons(season){
-        <TouchableOpacity key={key}>
-            <Text>{season.name}</Text>
-            <Image source={{ uri: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' + serie.poster_path }} />
-            <Text>{season.episode_count}</Text>
-            <Text>{season.air_date}</Text>
-        </TouchableOpacity>
+    renderSeasons(seasons){
+        //const { serie } = this.props.serie
+        //////////// /!\ CHANTIER /!\ et flatlist
+        return(
+            <TouchableOpacity style={{ flex: 1 }}>
+                    <View style={{ flex: 1 }}>
+                        <Image source={{ uri: 'https://image.tmdb.org/t/p/original/' + season.backdrop_path }} style={styles.imageSeason} />
+                        <Text style={styles.sectionContent}>{season.air_date}</Text>
+                        <Text style={styles.sectionContent}>{season.episode_count}</Text>
+                        <Text style={styles.sectionContent}>{season.name}</Text>
+                    </View>
+            </TouchableOpacity>
+        )
     }
 
     render() {
         const { serie } = this.props.serie
+        let firstDate = Moment(serie.first_air_date).format('YYYY')
+
         return (
-            <HeaderImageScrollView
-            maxHeight={200}
-            minHeight={50}
-            headerImage={{ uri: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' + serie.backdrop_path }}
-            >
-                <View style={{ height: 1000 }}>
-                    <TriggeringView onHide={() => console.log('text hidden')} >
-                    <Text>Scroll Me!</Text>
+            <View style={{ flex: 1 }}>
+                <StatusBar barStyle="light-content" />
+                <HeaderImageScrollView
+                    maxHeight={MAX_HEIGHT}
+                    minHeight={MIN_HEIGHT}
+                    maxOverlayOpacity={0.6}
+                    minOverlayOpacity={0.3}
+                    fadeOutForeground={true}
+                    renderHeader={() => <Image source={{ uri: 'https://image.tmdb.org/t/p/original/' + serie.backdrop_path}} style={styles.image} />}
+                    renderFixedForeground={() => (
+                        <Animatable.View
+                            style={styles.navTitleView}
+                            ref={navTitleView => {
+                                this.navTitleView = navTitleView;
+                            }}
+                        >
+                            <Text style={styles.navTitle}>{serie.name}</Text>
+                        </Animatable.View>
+                    )}
+                >
+                    <TriggeringView
+                        style={styles.section}
+                        onHide={() => this.navTitleView.fadeInUp(200)}
+                        onDisplay={() => this.navTitleView.fadeOut(100)}
+                    >
+                        <Text style={styles.title}>
+                            <Text style={styles.name}>{serie.name} , {firstDate}</Text>
+                        </Text>
                     </TriggeringView>
-                </View>
-            </HeaderImageScrollView>
-            /* <View style={styles.serie}>
-                <Image source={{ uri: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' + serie.backdrop_path }} style={styles.bgSerie} />
-                <Text>
-                    {this.props.serie.serie.name}
-                </Text>
-            </View> */
+                    <View style={styles.section}>
+                        <Text style={styles.sectionTitle}>Description</Text>
+                        <Text style={styles.sectionContent}>{serie.overview}</Text>
+                    </View>
+                    <View style={styles.section}>
+                        <Text style={styles.sectionTitle}>Caractéristiques</Text>
+                        <Text style={styles.sectionContent}>{serie.number_of_episodes}</Text>
+                        <Text style={styles.sectionContent}>{serie.number_of_seasons}</Text>
+                    </View>
+                    <View style={styles.section}>
+                        <Text style={styles.sectionTitle}>Saisons</Text>
+                        <FlatList
+                            data={serie.seasons}
+                            renderItem={({ season }) => this.renderSeasons(seasons)}
+                            keyExtractor={season => season.id}
+                            horizontal
+                        />
+                    </View>
+                    {/* <View style={[styles.section, styles.sectionLarge]}>
+                        <Text style={styles.sectionTitle}>Keywords</Text>
+                        <View style={styles.keywords}>
+                            <View style={styles.keywordContainer}>
+                                <Text style={styles.keyword}>Mots-clé de la série</Text>
+                            </View>
+                        </View>
+                    </View> */}
+                </HeaderImageScrollView>
+            </View>
         );
     }
 }
--- sources-dev/components/SeasonDetails/styles.js
+++ sources-dev/components/SeasonDetails/styles.js
@@ -1,11 +1,76 @@
 import { StyleSheet, Dimensions } from 'react-native';
+import { Header } from 'react-navigation';
+
+export const MIN_HEIGHT = Header.HEIGHT
+export const MAX_HEIGHT = 300
 
 export default StyleSheet.create({
-    bgSerie: {
-        width: "100%",
-        height: 200,
+    image: {
+        height: MAX_HEIGHT,
+        width: Dimensions.get('window').width,
+        alignSelf: 'stretch',
+        resizeMode: 'cover',
     },
-    serie: {
-        flex: 1
-    }
+    title: {
+        fontSize: 20,
+    },
+    name: {
+        fontWeight: 'bold',
+    },
+    section: {
+        padding: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc',
+        backgroundColor: 'white',
+    },
+    sectionTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    sectionContent: {
+        fontSize: 16,
+        textAlign: 'justify',
+    },
+    keywords: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexWrap: 'wrap',
+    },
+    keywordContainer: {
+        backgroundColor: '#999999',
+        borderRadius: 10,
+        margin: 10,
+        padding: 10,
+    },
+    keyword: {
+        fontSize: 16,
+        color: 'white',
+    },
+    titleContainer: {
+        flex: 1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageTitle: {
+        color: 'white',
+        backgroundColor: 'transparent',
+        fontSize: 24,
+    },
+    navTitleView: {
+        height: MIN_HEIGHT,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 16,
+        opacity: 0,
+    },
+    navTitle: {
+        color: 'white',
+        fontSize: 18,
+        backgroundColor: 'transparent',
+    },
+    sectionLarge: {
+        height: 600,
+    },
 });
